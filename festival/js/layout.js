2cd05403608c904c762a0317b710425eaf5fd970
jocelyn0107 <miracle3322@naver.com>
2025-02-14 12:59:28 +0900

    250214

--- festival/js/layout.js
+++ festival/js/layout.js
@@ -23,4 +23,43 @@ $(document).ready(function () {
 AOS.init();
 $(document).ready(function(){
     $('header>.left_menus, header>.right_menus, .header_logo').addClass('on');
+    $('.iphone_01, .iphone_02, .slogan').addClass('on');
+
+    $(window).on('scroll', function () {
+        const scrollTop = $(window).scrollTop();
+        const windowHeight = $(window).height();
+        const scrollBottom = scrollTop + windowHeight; // 화면 하단의 스크롤 위치
+        
+        $('.about_03').each(function(){
+            const about03Top = $(this).offset().top;
+            const starLine = $(this).find('.star_line');
+            const stars = $(this).find('.star_01');
+
+            if (scrollBottom >= about03Top) {
+                starLine.addClass('down');
+                setTimeout(() => {
+                    stars.addClass('show');
+                }, 970);
+            } else {
+                starLine.removeClass('down');
+                stars.removeClass('show');
+            }
+        });
+
+        $('.price_02').each(function(){
+            const price02Top = $(this).offset().top;
+            const starLine = $(this).find('.star_line');
+            const stars = $(this).find('.star_01');
+
+            if (scrollBottom >= price02Top) {
+                starLine.addClass('down');
+                setTimeout(() => {
+                    stars.addClass('show');
+                }, 970);
+            } else {
+                starLine.removeClass('down');
+                stars.removeClass('show');
+            }
+        });
+    });
 });
